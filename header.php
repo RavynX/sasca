141d8131a4451b56d4fdb16994805225e922a34e
Chris Leclair (home) <ravynx@gmail.com>
2014-03-24 22:53:52 -0500

    Adding viewport to css and header.

--- header.php
+++ header.php
@@ -1,8 +1,17 @@
 <!doctype html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8"/>
 		<title>SASCA - San Antonio Sports Car Association</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<meta name="description" content="SASCA - San Antonio Sports Car Association : The Autocrossing community from San Antonio, TX.">
+		<meta name="author" content="Chris Leclair">
+		<!--Mobile specific meta goodness :)-->
+	    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" type="text/css" href="css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		
+		<!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		
 		<script src="js/jquery-1.10.2.min.js"></script>
+		<!-- sasca.js script included in footer.php -->
 	</head>
