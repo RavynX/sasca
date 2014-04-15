cfaa42a320db5e384f1c0ac0744914e3b6e54912
Chris Leclair (home) <ravynx@gmail.com>
2014-04-15 18:23:28 -0500

    First time additions

--- firsttime.php
+++ firsttime.php
@@ -1,4 +1,27 @@
 <?php include('header.php') ?>
 <?php include('nav.php') ?>
-First Time?
+<div id="firsttime">
+	<p>Intro...</p>
+
+	<p class="heading">Items you'll want to bring</p>
+	<p class="desc">
+		<ul>
+			<li>DOT approved helmet if you have one.  We have loaner helmets to use</li>
+			<li></li>
+		</ul>
+	</p>
+
+	<p class="heading">Morning tasks to complete</p>
+	<p class="desc">
+		<ul>
+			<li>Register</li>
+			<li>Tape</li>
+			<li>Tech Inspection</li>
+		</ul>
+	</p>
+
+	<p class="heading">Novice Course Walk</p>
+	<p class="desc"></p>
+
+</div>
 <?php include('footer.php') ?>
