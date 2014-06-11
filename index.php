39e94fac239c00164883279fcc90a4c969e88830
Chris Leclair <ravynx@gmail.com>
2014-06-10 20:05:51 -0500

    Added content to front page. Modified CSS styling for front page and schedule page.

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php include('header.php') ?>
 <?php include('nav.php') ?>
-<div id="meeting">
-	<p>Monthly Meeting</p>
-	<p>Wednesday after the Event</p>
-	<p>Chat Starts at 6:45pm</p>
-	<p>Meeting at 7:30pm</p>
-	<p></p>
-	<p>
-		<a href="https://goo.gl/maps/4lY9l" target="_blank">Grady's Bar B Que<br/>
-		327 E Nakoma Dr<br/>
-		San Antonio, TX 78216</a>
-	</p>
-</div>
-Welcome to SASCA!
+<div id="sidebar">
+	<div id="event" class="action_item">
+		<p class="heading">Next Event</p>
+		<p class="description">Retama Park<br/>
+		June 22, 2014<br/>
+		<br/>
+		<a href="register.php">See the Schedule</a><br/>
+		<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11673" target="_blank">Click here to Register</a>
+		</p>
+	</div>
 
+	<div id="meeting" class="action_item">
+		<p class="heading">Monthly Meeting</p>
+		<p class="description">June 25th, 2014<br/>
+			Chat Starts at 6:45pm<br/>
+			Meeting at 7:30pm<br/>
+			<br/>
+			<a href="https://goo.gl/maps/4lY9l" target="_blank">Grady's Bar B Que<br/>
+			327 E Nakoma Dr<br/>
+			San Antonio, TX 78216</a>
+		</p>
+	</div>
+</div>
+<h1>Welcome to SASCA!</h1>
+<p>Thanks for swinging on by!  We're your local autocross club for the San Antonio, TX area.  Interested in seeing what we're about?  Come to our next event or one of our monthly meetings and introduce yourself.  It's free to spectate and many people offer their passenger seat for ride-alongs.</p>
+<br/>
+
+<h1>SCCA Solo National Championship Registration Opens</h1>
+<p class="date">June 1, 2014</p>
+<p>If you're interested in testing your skills against the top competitors in the U.S. then this is event to go to.  Held at <a href="https://goo.gl/maps/i0oR6" target="_blank">Lincoln, Nebraska Airport</a> this event brings over 1200 drivers to compete over 4 days on two separate courses, not including the ProSolo event the weekend prior.  This event will test your skills to see if you've got what it takes to bring home a Nationals Champion jacket.  SASCA and <a href="http://www.spokes.org/" target="_blank">Spokes</a> travel up there not just to compete but to have fun after the racing.  Register and come with us to see what you've been missing.</p>
+<br/>
 
 <?php include('footer.php') ?>
--- register.php
+++ register.php
@@ -5,91 +5,91 @@
 	<h2>2014 SASCA Schedule</h2>
 	<table id="events">
 		<tr>
-			<th class="number">#</th>
+			<th class="number">Points Event</th>
 			<th class="date">Date</th>
 			<th class="location">Location</th>
 			<th class="register">Register</th>
 			<th class="info">Info.</th>
 		</tr>
 		<tr>
-			<td>1. </td>
+			<td>#1 </td>
 			<td>January 26</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11667" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>2. </td>
+			<td>#2 </td>
 			<td>February 9</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11668" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>3. </td>
+			<td>#3 </td>
 			<td>March 2</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11670" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>4. </td>
+			<td>#4 </td>
 			<td>April 13</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11671" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>5. </td>
+			<td>#5 </td>
 			<td>May 18</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11672" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>6. </td>
+			<td>#6 </td>
 			<td>June 22</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11673" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>7. </td>
+			<td>#7 </td>
 			<td>July 20</td>
 			<td><a href="http://goo.gl/maps/gjKmn" target="_blank">San Antonio Raceway</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11737" target="_blank">Register Here</a></td>
 			<td class="info"><a href="http://www.spokes.org/" target="_blank">Spokes</a> hosted Co-Event</td>
 		</tr>
 		<tr>
-			<td>8. </td>
+			<td>#8 </td>
 			<td>August 10</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11674" target="_blank">Register Here</a></td>
 			<td class="info">SASCA hosted Co-Event with <a href="http://www.spokes.org/" target="_blank">Spokes</a></td>
 		</tr>
 		<tr>
-			<td>9. </td>
+			<td>#9 </td>
 			<td>September 21</td>
 			<td><a href="http://goo.gl/maps/gjKmn" target="_blank">San Antonio Raceway</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11741" target="_blank">Register Here</a></td>
 			<td class="info"><a href="http://www.spokes.org/" target="_blank">Spokes</a> hosted Co-Event</td>
 		</tr>
 		<tr>
-			<td>10. </td>
+			<td>#10 </td>
 			<td>October 12</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11676" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>11. </td>
+			<td>#11 </td>
 			<td>November 9</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11677" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
-			<td>12. </td>
+			<td>#12 </td>
 			<td>December 7</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
 			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11675" target="_blank">Register Here</a></td>
