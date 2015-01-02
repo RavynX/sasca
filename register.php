c188a0b3571bcfa108c18aa2776a71122810cba5
Chris Leclair <ravynx@gmail.com>
2015-01-01 22:42:45 -0600

    2015 Events published

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <?php include('nav.php') ?>
 <h1>Online Registration</h1><br/>
 <div id="register">
-	<h2>2014 SASCA Schedule</h2>
+	<h2>2015 SASCA Schedule</h2>
 	<table id="events">
 		<tr>
 			<th class="number">Points Event</th>
@@ -13,86 +13,86 @@
 		</tr>
 		<tr>
 			<td>#1 </td>
-			<td>January 26</td>
+			<td>January 11</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11667" target="_blank">Register Here</a></td>
+			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11940" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#2 </td>
-			<td>February 9</td>
+			<td>February 8</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11668" target="_blank">Register Here</a></td>
+			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11941" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#3 </td>
-			<td>March 2</td>
+			<td>March 22</td>
 			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11670" target="_blank">Register Here</a></td>
+			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11942" target="_blank">Register Here</a></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#4 </td>
-			<td>April 13</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11671" target="_blank">Register Here</a></td>
+			<td>April 19</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#5 </td>
-			<td>May 18</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11672" target="_blank">Register Here</a></td>
+			<td>May 17</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#6 </td>
-			<td>June 22</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11673" target="_blank">Register Here</a></td>
+			<td>June 28</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#7 </td>
-			<td>July 20</td>
-			<td><a href="http://goo.gl/maps/lrzOI" target="_blank">CoTA</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11737" target="_blank">Register Here</a></td>
-			<td class="info"><a href="http://www.spokes.org/" target="_blank">Spokes</a> hosted Co-Event (Must Prepay on Axware)</td>
+			<td>July 26</td>
+			<td>TBD</td>
+			<td></td>
+			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#8 </td>
-			<td>August 10</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11674" target="_blank">Register Here</a></td>
-			<td class="info">SASCA hosted Co-Event with <a href="http://www.spokes.org/" target="_blank">Spokes</a></td>
+			<td>August 16</td>
+			<td>TBD</td>
+			<td></td>
+			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#9 </td>
-			<td>September 21</td>
-			<td><a href="http://goo.gl/maps/gjKmn" target="_blank">San Antonio Raceway</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11741" target="_blank">Register Here</a></td>
-			<td class="info"><a href="http://www.spokes.org/" target="_blank">Spokes</a> hosted Co-Event</td>
+			<td>September 27</td>
+			<td>TBD</td>
+			<td></td>
+			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#10 </td>
-			<td>October 12</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11676" target="_blank">Register Here</a></td>
+			<td>October 18</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#11 </td>
-			<td>November 9</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11677" target="_blank">Register Here</a></td>
+			<td>November 15</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 		<tr>
 			<td>#12 </td>
-			<td>December 7</td>
-			<td><a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park</a></td>
-			<td><a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11675" target="_blank">Register Here</a></td>
+			<td>December 6</td>
+			<td>TBD</td>
+			<td></td>
 			<td class="info"></td>
 		</tr>
 	</table>
@@ -100,9 +100,9 @@
 
 <div id="faq">
 	<h1>Frequently Asked Questions</h1><br/>
-	
+
 	<h2>What payment types are accepted?</h2>
-	<p class="ans">SASCA accepts Cash, Checks, Debit, and Credit Cards.</p>	
+	<p class="ans">SASCA accepts Cash, Checks, Debit, and Credit Cards.</p>
 
 	<h2>Do I have to register online?</h2>
 	<p class="ans">No, you may register on-site the day of the event but it's easier on both you and us if you preregister online to streamline your time through checking in at the trailer in the morning.</p>
