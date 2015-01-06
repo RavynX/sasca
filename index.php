98e02255fc0eb9cb17118e6f10858d11f6a0eb35
Chris Leclair <ravynx@gmail.com>
2015-01-05 19:53:39 -0600

    Front page update for Awards Banquet

--- index.php
+++ index.php
@@ -4,24 +4,23 @@
 	<div id="event" class="action_item">
 		<p class="heading">Next Event</p>
 		<p class="description">Retama Park<br/>
-		December 7th, 2014<br/>
+		Valet Lot<br/>
+		January 11, 2015<br/>
 		<br/>
 		<a href="register.php">See the Schedule</a><br/>
-		<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11675" target="_blank">Click here to Register</a>
+		<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11940" target="_blank">Click here to Register</a>
 		</p>
 	</div>
 
 	<div id="meeting" class="action_item">
-		<p class="heading">Monthly Meeting</p>
-		<p class="description">November 12th, 2014<br/>
-			Chat Starts at 6:45pm<br/>
-			Meeting at 7:30pm<br/>
+		<p class="heading">2014 Awards Banquet</p>
+		<p class="description">January 17th, 2015<br/>
+			6:30pm - 9:30pm<br/>
+			<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11939&selectclubid=25&curmonthindex=0&curyearindex=9">Click Here for Details</a><br/>
+			<a href="http://goo.gl/maps/SU52D" target="_blank">Retama Park<br/>
+				1 Retama Parkway<br/>
+				Selma, TX 78154</a><br/>
 			<br/>
-			<a href="https://goo.gl/maps/4lY9l" target="_blank">Grady's Bar B Que<br/>
-			327 E Nakoma Dr<br/>
-			San Antonio, TX 78216</a><br/>
-			<br/>
-			Didn't get a chance to make it to the monthly meeting? <a href="http://www.sasca.org/forum/index.php/board,35.0.html" target="_blank">Click here</a> to view prior meeting minutes and see what's been going on.
 		</p>
 	</div>
 </div>
@@ -29,24 +28,14 @@
 <p style="margin-top: 8px;">Thanks for swinging on by!  We're your local autocross club for the San Antonio, TX area.  Interested in seeing what we're about?  Come to our next event or one of our monthly meetings and introduce yourself.  It's free to spectate and many people offer their passenger seat for ride-alongs.</p>
 <br/>
 
+<h1>2014 Awards Banquet!</h1>
+<p class="date">January 5, 2015</p>
+<p>It's just around the corner! Time to crown those year-end Champs, throw back a few, banter, and WIN SOME FABULOUS DOOR PRIZES! That's right, every adult who enters the party gets a ticket for entry into our door prize contest for this event.  Sign up ends January 5th. See you all there!</p>
+<br/>
+
 <h1>Board of Directors and Chair Elections!</h1>
 <p class="date">November 12, 2014</p>
 <p>It's that time of year again, to elect your 2015 BOD and nominate chair positions.  The BOD committe is selected by the club members present at the December 2014 meeting and the chair positions are elected by the existing BOD.  Tonight we will be having our November meeting to discuss this and the SASCA By Laws further.  Come on out!</p>
-<br/>
-
-<h1>Spokes Co-Hosted Event at CoTA, July 20th!</h1>
-<p class="date">July 15, 2014</p>
-<p>Spokes is hosting their July 20th event at Circuit of the Americas in Lot H!  The event is limited to 160 entrants so you must register and pre-pay on Axware.  Other limitations include (1) must be at least 18 years old with a valid drivers license (2) Entry fee of $36 for members or $41 for non-members (3) you must register and pre-pay on Axware (no refunds or walk ups) (4) and lastly no Double entries are allowed at this time but may open up later this week.  Check the <a href="register.php">Event Sign-Up page</a> for more information.</p>
-<br/>
-
-<h1>Points Event #6 Venue Lot Change</h1>
-<p class="date">June 20, 2014</p>
-<p>Attenion everyone: If you are planning to attend SASCA Points Event #6 out at Retama please be advised that for this event in particular Retama staff has asked the club to run the event in the vallet lot on the East side of the venue due to the horse races.  Please click the image for a full view.<br/><br/><a href="images/retama_revised.jpg" target="_blank"><img src="images/retama_revised.jpg" height="200px;" /></a></p>
-<br/>
-
-<h1>SCCA Solo National Championship Registration Opens</h1>
-<p class="date">June 1, 2014</p>
-<p>If you're interested in testing your skills against the top competitors in the U.S. then this is event to attend.  Held at <a href="https://goo.gl/maps/i0oR6" target="_blank">Lincoln, Nebraska Airport</a>, this event brings over 1200 drivers to compete over 4 days on two separate courses, not including the ProSolo event the weekend prior.  This event will test your skills to see if you've got what it takes to bring home a Nationals Champion jacket.  SASCA and <a href="http://www.spokes.org/" target="_blank">Spokes</a> travel up there not just to compete but to have fun after the racing.  Sign up and come with us to see what you've been missing!</p>
 <br/>
 
 <?php include('footer.php') ?>
