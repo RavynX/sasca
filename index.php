ab72f1ac592c5ebb3b4a7a1f34cddf28528502b1
Chris Leclair <ravynx@gmail.com>
2015-02-04 21:44:20 -0600

    Moved twitter feed to the left side of the page

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php include('header.php') ?>
 <?php include('nav.php') ?>
-<div id="sidebar">
-	<div id="event" class="action_item">
+<div id="sidebar_left">
+	<div id="twitter_feed">
+		<a class="twitter-timeline" href="https://twitter.com/sascaracing" data-widget-id="555167603816022016">Tweets by @sascaracing</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		</div>
+		<div id="content">
+		<h1>Welcome to SASCA!</h1>
+		<p style="margin-top: 8px;">Thanks for swinging on by!  We're your local autocross club for the San Antonio, TX area.  Interested in seeing what we're about?  Come to our next event or one of our monthly meetings and introduce yourself.  It's free to spectate and many people offer their passenger seat for ride-alongs.</p>
+		<br/>
+
+		<h1>2014 Awards Banquet!</h1>
+		<p class="date">January 5, 2015</p>
+		<p>It's just around the corner! Time to crown those year-end Champs, throw back a few, banter, and WIN SOME FABULOUS DOOR PRIZES! That's right, every adult who enters the party gets a ticket for entry into our door prize contest for this event.  Sign up ends January 5th. See you all there!</p>
+		<br/>
+
+		<h1>Board of Directors and Chair Elections!</h1>
+		<p class="date">November 12, 2014</p>
+		<p>It's that time of year again, to elect your 2015 BOD and nominate chair positions.  The BOD committe is selected by the club members present at the December 2014 meeting and the chair positions are elected by the existing BOD.  Tonight we will be having our November meeting to discuss this and the SASCA By Laws further.  Come on out!</p>
+		<br/>
+		</div>
+		<div id="sidebar_right">
+		<div id="event" class="action_item">
 		<p class="heading">Next Event</p>
 		<p class="description">Retama Park<br/>
 		Lot 4<br/>
@@ -10,38 +31,22 @@
 		<a href="register.php">See the Schedule</a><br/>
 		<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=11941" target="_blank">Click here to Register</a>
 		</p>
-	</div>
+		</div>
 
-	<div id="meeting" class="action_item">
+		<div id="meeting" class="action_item">
 		<p class="heading">February Meeting</p>
-			<p class="description">February 11th, 2015<br/>
-			Chat Starts at 6:45pm<br/>
-			Meeting at 7:30pm<br/>
-			<br/>
-			<a href="https://goo.gl/maps/4lY9l" target="_blank">Grady's Bar B Que<br/>
-			327 E Nakoma Dr<br/>
-			San Antonio, TX 78216</a><br/>
-			<br/>
-			Didn't get a chance to make it to the monthly meeting? <a href="http://www.sasca.org/forum/index.php/board,35.0.html" target="_blank">Click here</a> to view prior meeting minutes and see what's been going on.
+		<p class="description">February 11th, 2015<br/>
+		Chat Starts at 6:45pm<br/>
+		Meeting at 7:30pm<br/>
+		<br/>
+		<a href="https://goo.gl/maps/4lY9l" target="_blank">Grady's Bar B Que<br/>
+		327 E Nakoma Dr<br/>
+		San Antonio, TX 78216</a><br/>
+		<br/>
+		Didn't get a chance to make it to the monthly meeting? <a href="http://www.sasca.org/forum/index.php/board,35.0.html" target="_blank">Click here</a> to view prior meeting minutes and see what's been going on.
 		</p>
-	</div>
-	<div id="twitter_feed">
-		<a class="twitter-timeline" href="https://twitter.com/sascaracing" data-widget-id="555167603816022016">Tweets by @sascaracing</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
-</div>
-<h1>Welcome to SASCA!</h1>
-<p style="margin-top: 8px;">Thanks for swinging on by!  We're your local autocross club for the San Antonio, TX area.  Interested in seeing what we're about?  Come to our next event or one of our monthly meetings and introduce yourself.  It's free to spectate and many people offer their passenger seat for ride-alongs.</p>
-<br/>
+		</div>
+		</div>
 
-<h1>2014 Awards Banquet!</h1>
-<p class="date">January 5, 2015</p>
-<p>It's just around the corner! Time to crown those year-end Champs, throw back a few, banter, and WIN SOME FABULOUS DOOR PRIZES! That's right, every adult who enters the party gets a ticket for entry into our door prize contest for this event.  Sign up ends January 5th. See you all there!</p>
-<br/>
-
-<h1>Board of Directors and Chair Elections!</h1>
-<p class="date">November 12, 2014</p>
-<p>It's that time of year again, to elect your 2015 BOD and nominate chair positions.  The BOD committe is selected by the club members present at the December 2014 meeting and the chair positions are elected by the existing BOD.  Tonight we will be having our November meeting to discuss this and the SASCA By Laws further.  Come on out!</p>
-<br/>
-
-<?php include('footer.php') ?>
+		<?php include('footer.php') ?>
+		
