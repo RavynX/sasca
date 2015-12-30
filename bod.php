0aa5bd0b267ea753941e0446c79e4f50dcf68615
Chris Leclair <Chris Leclair>
2015-12-30 11:43:00 -0600

    2016 BoD and Chair changes

--- bod.php
+++ bod.php
@@ -1,37 +1,37 @@
 <?php include 'index.php' ?>
 <?php startblock('content') ?>
 <div id="bod">
-	<h1>2014 Board of Directors</h1>
+	<h1>2016 Board of Directors</h1>
 	<table class="chairs">
 		<tr>
 			<td class="title">President</td>
 			<td class="name">Chris Leclair</td>
-			<td class="email"><a href="mailto:ravynx@gmail.com">e-mail</a></td>
+			<td class="email"><a href="mailto:president@sasca.org">e-mail</a></td>
 		</tr>
 		<tr>
 			<td class="title">Vice President</td>
 			<td class="name">Diane Stevens</td>
-			<td class="email"><a href="mailto:miatagirl576@gmail.com">e-mail</a></td>
+			<td class="email"><a href="mailto:vicepresident@sasca.org">e-mail</a></td>
 		</tr>
 		<tr>
 			<td class="title">Treasurer</td>
 			<td class="name">Michael Roetheli</td>
-			<td class="email"><a href="mailto:michaelroetheli@sbcglobal.net">e-mail</a></td>
+			<td class="email"><a href="mailto:treasurer@sasca.org">e-mail</a></td>
 		</tr>
 		<tr>
 			<td class="title">Secretary</td>
 			<td class="name">Jason Marshall</td>
-			<td class="email"><a href="mailto:Jmarshall25@gmail.com">e-mail</a></td>
+			<td class="email"><a href="mailto:secretary@sasca.org">e-mail</a></td>
 		</tr>
 		<tr>
 			<td class="title">Past President</td>
 			<td class="name">Ricky Crow</td>
-			<td class="email"><a href="mailto:rickycrx@gmail.com">e-mail</a></td>
+			<td class="email"><a href="mailto:bod@sasca.org">e-mail</a></td>
 		</tr>
 	</table>
 	<br/>
 	<br/>
-	<h1>2014 Chairs</h1>
+	<h1>2016 Chairs</h1>
 	<table class="chairs">
 		<tr>
 			<td class="title">Membership</td>
@@ -39,7 +39,7 @@
 		</tr>
 		<tr>
 			<td class="title">Novice Chief</td>
-			<td class="name">Jason Kupka</td>
+			<td class="name">Brian Burdette</td>
 		</tr>
 		<tr>
 			<td class="title">Safety Steward</td>
@@ -55,7 +55,7 @@
 		</tr>
 		<tr>
 			<td class="title">Tech Inspection</td>
-			<td class="name">Alyssa Roetheli &nbsp; Erika Robinson</td>
+			<td class="name">Alyssa Roetheli &nbsp; Caitlin Roetheli</td>
 		</tr>
 		<tr>
 			<td class="title">Timing and Scoring</td>
@@ -63,7 +63,7 @@
 		</tr>
 		<tr>
 			<td class="title">Worker Chief</td>
-			<td class="name">Jon Parkoff</td>
+			<td class="name">Adam White</td>
 		</tr>
 		<tr>
 			<td class="title">Webmaster</td>
