deead684fd385df89f5b6341371555f85b09b43e
Chris Leclair <ravynx@gmail.com>
2016-03-28 10:08:08 -0500

    Front page April update

--- home.php
+++ home.php
@@ -30,18 +30,18 @@
 				<div id="event" class="action_item">
 				<p class="heading">Next Event</p>
 					<p class="description">RCCC<br/>
-						Saturday, Event #2<br/>
-						February 20th, 2016<br/>
+						Saturday, Event #4<br/>
+						April 9th, 2016<br/>
 						<br/>
 						<a href="register.php">See the Schedule</a><br/>
-						<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=12279" target="_blank">Click here to Register</a>
+						<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=12313" target="_blank">Click here to Register</a>
 					</p>
 				</div>
 
 				<div id="meeting" class="action_item">
 					<p class="heading">Next Meeting</p>
 					<p class="description">Wednesday<br/>
-						February 24th, 2016<br/>
+						April 13th, 2016<br/>
 						Chat &amp; Food Starts at 6:30pm<br/>
 						Meeting starts at 7:30pm<br/>
 						<br/>
