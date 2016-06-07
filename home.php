f9fcc11b3d0bb4ba57d18b1264051ef7f623314e
Chris Leclair <ravynx@gmail.com>
2016-06-06 21:52:01 -0500

    Updating for June

--- home.php
+++ home.php
@@ -3,6 +3,22 @@
 			<p style="margin-top: 8px;">Thanks for swinging on by!  We're your local autocross club for the San Antonio, TX area.  Interested in seeing what we're about?  Come to our next event or one of our monthly meetings and introduce yourself.  It's free to spectate and many people offer their passenger seat for ride-alongs.</p>
 			<br/>
 
+			<h1>Spokes/SASCA Mini-Divisional - June 25/26</h1>
+			<p class="date">June 6, 2016</p>
+			<p>Spokes and SASCA will be hosting autocrosses on back to back days. Spokes is taking this opportunity to setup a divisional event that will mimic national level SCCA events.</p>
+			<p>For those unfamiliar with national level events, the competition spans 2 days of racing and your best times from each day are combined for competition.</p>
+			<p>We are still working on some details but here is what we have so far:
+				<ul style="list-style-type:circle; padding-left: 30px;">
+					<li>The number of runs at each event will remain unchanged. (This gets decided based on how quickly the first heat progresses. We typically get 4 - 6 runs per event.)</li>
+					<li>The number of runs that will factor into the weekend long divisional competition is TBD.</li>
+					<li>There will be prizes for class winners of the weekend long competition as well as prizes for other notable performances.</li>
+					<li>We are planning to have a social event on the evening of Saturday, June 25th.</li>
+				</ul></p>
+				<p>SASCA will be hosting on Saturday, June 25 @ River City Community Church</p>
+				<p>Spokes will be hosting on Sunday, June 26 @ San Antonio Raceway</p>
+			</p>
+			<br/>
+
 			<h1>SASCA End of Year Awards Party</h1>
 			<p class="date">December 12th, 2015</p>
 			<p>The SASCA year-end party and awards banquet will be taking place at the Blue Star Brewery at 1414 S Alamo St, San Antonio, TX. 78210.  Come hungry and join us for dinner and awards presentation at 6:00pm on Saturday, January 30, 2015.<br/>
@@ -30,18 +46,18 @@
 				<div id="event" class="action_item">
 				<p class="heading">Next Event</p>
 					<p class="description">RCCC<br/>
-						Saturday, Event #5<br/>
-						May 21st, 2016<br/>
+						Saturday, Event #6<br/>
+						June 25th, 2016<br/>
 						<br/>
 						<a href="register.php">See the Schedule</a><br/>
-						<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=12314" target="_blank">Click here to Register</a>
+						<a href="https://axwaresystems.com/axorm/calendar_main.php?viewevent=12315" target="_blank">Click here to Register</a>
 					</p>
 				</div>
 
 				<div id="meeting" class="action_item">
 					<p class="heading">Next Meeting</p>
-					<p class="description">Monday<br/>
-						May 23rd, 2016<br/>
+					<p class="description">Wednesday<br/>
+						June 29th, 2016<br/>
 						Chat &amp; Food Starts at 6:30pm<br/>
 						Meeting starts at 7:30pm<br/>
 						<br/>
