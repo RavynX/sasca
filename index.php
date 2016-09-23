fd2b513b6db1ac76c2302c39aa74c6384bc2fa21
Chris Leclair <ravynx@gmail.com>
2016-09-22 21:27:04 -0500

    Updating Fonts with Diane.

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<!-- Fonts -->
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Libre+Baskerville">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans" rel="stylesheet">
 
 		<!-- Styles -->
 		<link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
