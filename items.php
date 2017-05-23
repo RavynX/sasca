b45dafee36fec449a0d184a62a3d00e861984497
Chris Leclair <ravynx@gmail.com>
2017-05-22 21:45:12 -0500

    Relinked Novice Participation and Items pages.  Reorganized page layouts for those two.

--- items.php
+++ items.php
@@ -1,5 +1,17 @@
 <?php include 'index.php' ?>
 <?php startblock('content') ?>
-What items should I bring?
+<h1>What items should I bring?</h1>
+<p class="desc">
+  <ul class="disc spacing">
+    <li><b>DOT-approved helmet</b> if you have one.  We have loaner helmets to use if you don't have one. Please return them when you're done with your runs.</li>
+    <li><b>Water</b> : Bring plenty of water to stay hydrated through the day.  We'll have water for sale if you don't have any.</li>
+    <li><b>Sunscreen, Hats, Umbrellas</b> or whatever you prefer for protection from the sun.  Texas summers get toasty, so bring what you need to keep your skin from burning.</li>
+    <li><b>Wind jacket</b> or <b>rain coat</b> if inclement weather is expected.  Texas weather changes throughout the day so it's good to pack these just in case.</li>
+    <li><b>Tire Gauge</b> : We have an air compressor on site to fill your tires.  Bring a tire gauge to check and monitor tire pressures.</li>
+    <li><b>Tools</b> : Pushing your car to the limits will start to expose weak points.  You may need to adjust things that loosen so it's good to have tools to fix things.</li>
+    <li><b>Quart of Oil</b> : Make sure to check your oil level before heading to the event but bring a spare quart if your car is prone to burning oil.</li>
+    <li><b>A copy of the SCCA Rulebook</b>:  <a target="_blank" href="https://www.scca.com/pages/solo-cars-and-rules">Click Here for SCCA Solo Rulebooks</a>.</li>
+  </ul>
+</p>
 
 <?php endblock() ?>
