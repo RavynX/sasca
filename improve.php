e67cdeb7d7cebe5efcbd4448328b7a5206390c7f
Chris Leclair <ravynx@gmail.com>
2017-05-22 23:31:53 -0500

    Added Autox Improvement links

--- improve.php
+++ improve.php
@@ -1,7 +1,28 @@
 <?php include 'index.php' ?>
 <?php startblock('content') ?>
 <div id="improve">
-	<p>Improving your Skills</p>
+	<h1>Improving your Skills</h1>
+
+	<h3>Websites</h3>
+	<ul class="disc">
+		<li><a href="http://www.beyondseattime.com/" target="_blank">Beyond Seat Time (Vivek Goel)</a></li>
+		<li><a href="https://forum.miata.net/vb/showthread.php?t=44932" target="_blank">Andy Hollis' Top Ten Autox Driving Tips</a></li>
+		<li><a href="http://farnorthracing.com/autocross_secrets.html" target="_blank">Autocross to Win (Dennis Grant)</a></li>
+	</ul>
+
+	<h3>Books</h3>
+	<ul class="disc">
+		<li><a href="https://www.amazon.com/Winning-Autocross-Techniques-Speed-Secrets/dp/0760331561" target="_blank">Winning Autocross Techniques (Speed Secrets)</a></li>
+		<li><a href="https://www.amazon.com/Secrets-Solo-Racing-Techniques-Autocross/dp/0962057312" target="_blank">Secrets of Solo Racing - Henry Watts</a></li>
+		<li><a href="https://www.amazon.com/exec/obidos/ASIN/0965160009/thedsmautocropag" target="_blank">Drive to Win - Carroll Smith</a></li>
+	</ul>
+
+	<h3>Programs</h3>
+	<ul class="disc">
+		<li><a href="http://evoschool.com/" target="_blank">Evolution Performance Driving School</a></li>
+		<li><a href="https://www.scca.com/pages/starting-line" target="_blank">SCCA Starting Line School</a></li>
+		<li><a href="http://skipbarber.com/high-performance-driving-schools/" target="_blank">Skip Barber Racing School</a></li>
+	</ul>
 
 
 </div>
