526a6b3ce7cd4c4147a7b59cbf4651c953c9177b
Chris Leclair <ravynx@gmail.com>
2017-05-22 23:35:40 -0500

    Added What is Autocross content

--- autocross.php
+++ autocross.php
@@ -1,5 +1,9 @@
 <?php include 'index.php' ?>
 <?php startblock('content') ?>
-What is Autocross?
+<h1>What is Autocross?</h1>
+
+<p>Driving your car on an enclosed course made of cones typically in a parking lot against the clock.</p>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Af2zJAOAqWQ" frameborder="0" allowfullscreen></iframe>
 
 <?php endblock() ?>
