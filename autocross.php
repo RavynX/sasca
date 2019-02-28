fcc173d56c032412eb9a38e7afc673c16936936c
Chris Leclair <223844+RavynX@users.noreply.github.com>
2019-02-27 20:15:28 -0600

    Updated results_filename values for sasca.json file and changed wording on autocross.php page.

--- autocross.php
+++ autocross.php
@@ -2,7 +2,8 @@
 <?php startblock('content') ?>
 <h1>What is Autocross?</h1>
 
-<p>A form of motorsport where you're racing against the clock on a tighter/smaller temporary road course constructed from cones and sometimes outlined with chalk.</p>
+<p>Solo is the SCCA brand name for autocross competition. Autocross events are driving skill contests that emphasize the driver's ability and the car's handling characteristics. This is accomplished by driving a course that is designated by traffic cones on a low hazard location, such as a parking lot or inactive airstrip. While speeds are no greater than those normally encountered in legal highway driving, the combination of concentration and car feedback creates an adrenaline pumping experience. The runs are timed down to a thousanth of a second.
+</p>
 
 <div class="videoWrapper">
   <iframe src="https://www.youtube.com/embed/Af2zJAOAqWQ" frameborder="0" allowfullscreen></iframe>
