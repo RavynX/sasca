01ecd7cbb5f9ed492f3ccf0e9b9fa1f5bb68dab7
Chris Leclair <ravynx@gmail.com>
2019-10-01 18:02:15 -0500

    Changed schedule.php to display 'TBD' for unknown event dates.  Updated for Event 10 which is in limbo.

--- schedule.php
+++ schedule.php
@@ -14,7 +14,14 @@
 
 				echo "<tr>";
 				echo "<td>#".$event['id']."</td>";
-				echo "<td>".date_format((new DateTime($event['date'])), 'M j').'</td>';
+				if ($event['date'] == '') {
+					echo "<td>TBD</td>";
+				}
+				else {
+					echo "<td>".date_format((new DateTime($event['date'])), 'M j').'</td>';
+				}
+
+
 				echo "<td><a href='".$event['map_url']."' target='_blank'>".$event['site']."</a></td>";
 				if ($event['axware_id'] == 0) {
 					echo '<td>TBD</td>';
