dae596384c250390bef7224565c3bdb70a41601d
Chris Leclair <ravynx@gmail.com>
2021-06-10 23:36:49 -0500

    Covid Guideline Updates, June 10, 2021.

--- covid.php
+++ covid.php
@@ -2,31 +2,24 @@
 <?php startblock('content') ?>
 <div id="covid">
   <h1>SASCA COVID-19 Event Procedures</h1>
-  <p>Updated: January 27, 2021</p>
+  <p>Updated: June 10, 2021</p>
   <p class="blue">All updates are highlighted in blue font.</p>
   <p>In order to provide a safe environment for all event participants, SASCA is modifying our event procedures to best align with local and state guidelines to keep our communities safe during the COVID-19 pandemic.</p>
   <p class="red">We encourage everyone to maintain 6 feet of social distancing at all times throughout the event.</p>
   <table>
     <tr>
-      <th colspan="2">NEW AND MODIFIED EVENT PROCEDURES</td>
+      <th colspan="2">MODIFIED EVENT PROCEDURES</td>
+    </tr>
+    <tr>
+      <td>Latest Update</td>
+      <td>
+        <p class="blue">To keep in alignment with the Texas Governor's Open Texas 100% Efforts and CDC Guidance vaccinated individuals do not need to wear a mask. Non-vaccinated individuals are encouraged to wear a mask. Please respect others that choose to wear a mask.</p>
+        <p class="blue">Unless otherwise specified below, event procedures are back to normal.</p>
+      </td>
     </tr>
     <tr>
       <td>Sanitization</td>
       <td>Wash/sanitize your hands regularly, keep cross-contamination in mind. Additional sanitation stations will be provided onsite.</td>
-    </tr>
-    <tr>
-      <td>Event Limit & Spectators</td>
-      <td>
-        <p>There is no limit on event participants (drivers or spectators).</p>
-        <p>Members and non-members are allowed.</p>
-    </tr>
-    <tr>
-      <td>Payment & Registration</td>
-      <td>
-        <p>Pre-registration is required to receive the member discount.</p>
-        <p>Walkup-up/day-of registrations allowed. Note that payment for walk up registrations will be taken in person.</p>
-        <p>Pre-payment is available on Axware if desired, with a <b><u>NO REFUND</u></b> clause.</p>
-        <p>Doubles registration is allowed under normal provisions, which can be found here: <a href="https://www.sasca.org/doubles.php" target="_blank">https://www.sasca.org/doubles.php</a></p></td>
     </tr>
     <tr>
       <td>Loaner Helmet Program</td>
@@ -35,74 +28,11 @@
         <p>SASCA has ordered a limited number of helmets for purchase at the event.</p></td>
     </tr>
     <tr>
-      <td>Waiver Signing</td>
-      <td>
-        <p class="blue">Gate Workers will be present before you enter the paddock to have you sign the waiver and hand out wrist bands.</p>
-      </td>
-    </tr>
-    <tr>
-      <td>On Course Passengers</td>
-      <td>
-        <p class="blue">RIDE-ALONGS ARE NOW ALLOWED. Masks or full face helmets are mandatory while in the car and windows are to be rolled down.</p>
-        <p class="blue">This includes novice ride-alongs, co-drivers, and personal ride-alongs, subject to regular rules for age 16+ driver license holders only.</p>
-      </td>
-    </tr>
-    <tr>
-      <td>Grid</td>
-      <td>
-        <p class="blue"><b>DO NOT GATHER IN GRID; DRIVERS AND RIDERS ONLY.</b></p>
-        <p>Masks must be worn while in grid (while outside of your car).</p>
-        <p>Grid will be spaced out extra-wide to better accommodate social distancing.</p>
-      </td>
-    </tr>
-    <tr>
-      <td>Food Truck</td>
-      <td>
-        <p>The following procedures have been laid out by the <a href="https://www.facebook.com/Eddieshijoesumobilecafe" target="_blank">Hijo-e-su</a> food truck:</p>
-        <div class="sub-info">
-          <p>We follow the same safety food handling guidelines as always. We implemented a couple of changes to follow regulations:</p>
-          <ol>
-            <li>Condiments are issued upon request only</li>
-            <li>Pre-wrapped utensils are distributed</li>
-            <li>Contactless Payment methods are available (Apple/Google Pay)</li>
-            <li>A sign is posted on truck to practice Social Distancing</li>
-          </ol>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>Registration</td>
-      <td>
-        <p>Check-in: In order to support social distancing for trailer workers, we will have:</p>
-        <div class="sub-info">
-          <ul>
-            <li>Two registration workers inside the trailer as normal.</li>
-            <li>One registration worker set up at a table at the backside of the trailer for cash payments.</li>
-          </ul>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>Tech Inspection</td>
-      <td>
-        <p>BE READY TO GO WITH CLASS AND NUMBERS BEFORE LINING UP FOR TECH.</p>
-        <p>To prevent too many people buzzing around in Tech, you will need to be ready with numbers and class tape/decals/magnets.  If not, you will be sent back to prep in paddock.</p>
-        <p>STAY IN YOUR CAR and let the tech workers inspect the car.  Be prepared for them to ask you to check your throttle, etc.</p>
-    </td>
-    </tr>
-    <tr>
       <td>Trailer Work</td>
       <td>
-        <p>Trailer Workers: Please bring your own masks.  Gloves will be provided.</p>
+        <p class="blue">Trailer Workers: If you choose to wear a mask, please bring your own. Gloves will be provided if requested.</p>
         <p>Timing and Radio will be setup inside the trailer.</p>
         <p>Announcer will be set up with a separate screen socially distant from Timing and Radio workers.</p>
-      </td>
-    </tr>
-    <tr>
-      <td>Course Walk</td>
-      <td>
-        <p>PRACTICE SOCIAL DISTANCING</p>
-        <p>Remain 6 feet apart from other event participants other than immediate family or individuals in your same household.</p>
       </td>
     </tr>
     <tr>
@@ -111,30 +41,7 @@
         <div class="sub-info">
           <ul>
             <li>Novice Walk through will be conducted at the same time but in small groups with a few different coaches.</li>
-            <li class="blue">Note: Co-drivers and ride-alongs are now allowed. Masks or full face helmets are mandatory while in the car and windows are to be rolled down.</li>
-          </ul>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>Driver Meeting</td>
-      <td>
-        <p>DO NOT GATHER AT THE TRAILER</p>
-        <p>You may listen to the meeting in paddock via the SASCA speakers or on your radio.</p>
-        <p>PAY ATTENTION TO RUN/WORK ORDER DURING THE MEETING</p>
-        <p>We will attempt to post the Run/Work order in the Facebook Group and SASCA.org.</p>
-        <p>If this is not possible participants will need to practice social distancing at the trailer.</p>
-      </td>
-    </tr>
-    <tr>
-      <td>Course Work</td>
-      <td>
-        <p>PRACTICE SOCIAL DISTANCING</p>
-        <div class="sub-info">
-          <ul>
-            <li>Starter – check wristbands & helmet straps to make sure they are properly secured.</li>
-            <li>Grid – run as normal, be cognizant of distance from others.</li>
-            <li>On-Course Workers – spread out and keep distance from other workers.</li>
+            <li class="blue">Note: Co-drivers and ride-alongs are allowed.  Please see Latest Update at the top for mask guidance.</li>
           </ul>
         </div>
       </td>
@@ -145,19 +52,14 @@
     <tr>
       <td colspan="2">
         <p>Q: Do event participants have to wear a mask at the event?</p>
-        <p>A: If you read to the end of this page, there are references at the bottom. You will find a link to the Governor's Open Texas Page that provides the following guidance for Outdoor Motorsports: </p>
-        <p class="italic">"Maintain at least 6 feet of separation from other individuals not within the same household. If such distancing is not feasible, other measures such as hand hygiene, cough etiquette, cleanliness, and sanitation should be rigorously practiced. Face shields may be considered as additional source control either in addition to face coverings, or where face coverings are not feasible. Considerations must be made regarding safety of face coverings during physical exertion or in hot environments.”</p>
-        <p class="blue">SASCA requires face coverings be worn while in the SASCA trailer, in grid, and while riding in a car with a person who is not in your household.</p>
-        <p>SASCA also provides consideration, according to the above, regarding safety of face coverings during physical exertion or in hot environments. Our event venue provides enough open space to allow for 6 ft of social distancing for those not in the SASCA trailer and in grid. The venue will also be in an open parking lot where the weather will be hot. Therefore, we are not requiring face coverings at our events for any situation outside of the above listed situations.</p>
+        <p>A: See Latest Update at the top for mask guidance.</p>
       </td>
     </tr>
 </table>
 <p></p>
 <p class="italic">Note: We will continue to monitor changes in local and state guidelines and modify processes/procedures as needed to hold the event.</p>
 <p>References:</p>
-<p><a href="https://www.whitehouse.gov/openingamerica/" target="_blank">White House Guidelines for Opening up America Again</a></p>
 <p><a href="https://gov.texas.gov/organization/opentexas" target="_blank">Governor’s Strike Force to Open Texas</a></p>
-<p><a href="https://www.bexar.org/3234/COVID-19" target="_blank">Bexar County COVID-19 Information</a></p>
 </div>
 
 <?php endblock() ?>
