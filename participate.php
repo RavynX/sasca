656a70a0db7f26ca1bba85f42770109993ee75c4
Chris Leclair <ravynx@gmail.com>
2023-06-09 09:10:41 -0500

    Adjustment to participate.php for letters on cars

--- participate.php
+++ participate.php
@@ -18,7 +18,7 @@
     <ul class="disc">
       <li><u>Sign the Waiver</u>: As soon as you arrive on site either at the gate or the trailer.  This includes both drivers and spectators.</li>
       <li><u>Register</u> : As soon as you arrive on site you'll want to go check in at the trailer.  The people working registration will get you entered into the computer and help find out what class your car falls under.</li>
-      <li><u>Tape</u> :  After you've registered and gotten your car Class and Number you'll want to label your car as such.  Numbers should be at least 8" tall and letters should be at least 4" tall.  Don't use blue tape on a blue car, white tape on a white car, etc.  Make sure the numbers and letters are legible and don't make a 5 look like an S and vice versa.  We have tape provided at the event either in the trailer or in Tech Inspection.</li>
+      <li><u>Tape</u> :  After you've registered and gotten your car Number and Class you'll want to label your car as such. Numbers should be at least 8" tall and letters should be at least 4" tall. There are computer printouts of numbers, letters and tape in Tech Inspection. You will need to tape a set of these to each side of your car.</li>
       <li><u>Prepparing your car</u> : You will need to remove the driver's side floor mat and remove all loose items in the car before heading over to Tech Inspection.
       <li><u>Tech Inspection</u> : Once you've registered and added your Number and Class to your car you'll want to bring your car over to the Tech Inspection area where they'll check for items such as wheel bearing, seat belts, gas pedal return, etc.  You can view the full list <a href="tech.php">here</a>.</li>
     </ul>
